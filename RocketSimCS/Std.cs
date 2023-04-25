133e7a63d066cb929bb8e208667654eb4889f841
unknown <jboardman367@gmail.com>
2023-04-25 17:23:27 +1000

    Fixes to auto-generated bindings

--- RocketSimCS/Std.cs
+++ RocketSimCS/Std.cs
@@ -295,18 +295,6 @@ namespace Std
 
 namespace Std
 {
-}
-
-namespace Std
-{
-}
-
-namespace Std
-{
-}
-
-namespace Std
-{
     namespace Function
     {
         [StructLayout(LayoutKind.Sequential, Size = 64, Pack = 8)]
@@ -315,8 +303,30 @@ namespace Std
             internal global::Std.FuncClass.Storage.__Internal _Mystorage;
         }
     }
-}
 
-namespace Std
-{
+    namespace FuncClass
+    {
+        [StructLayout(LayoutKind.Sequential, Size = 64, Pack = 8)]
+        public unsafe partial struct __Internal
+        {
+            internal global::Std.FuncClass.Storage.__Internal _Mystorage;
+        }
+
+        namespace Storage
+        {
+            [StructLayout(LayoutKind.Explicit, Size = 64, Pack = 8)]
+            public unsafe partial struct __Internal
+            {
+                [FieldOffset(0)]
+                internal double _Dummy1;
+
+                [FieldOffset(0)]
+                internal fixed sbyte _Dummy2[56];
+
+                [FieldOffset(0)]
+                internal fixed long _Ptrs[8];
+            }
+        }
+
+    }
 }
